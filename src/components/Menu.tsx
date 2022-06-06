c62a01ef7b7d951d9f3e49c5d51c5e27b3a09e61
Mark Burggraf <markb@mantisbible.com>
2022-06-06 09:05:39 -0700

    scaffold menu, data service, misc functions

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,4 +1,5 @@
 import {
+  IonButton,
   IonContent,
   IonIcon,
   IonItem,
@@ -9,92 +10,161 @@ import {
   IonMenuToggle,
   IonNote,
 } from '@ionic/react';
+import { Login, ResetPassword, User, SupabaseAuthService} from 'ionic-react-supabase-login';
+import { useEffect, useMemo, useState } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
+import { archiveOutline, archiveSharp, barChartOutline, barChartSharp, bookmarkOutline, heartOutline, heartSharp, mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, peopleOutline, peopleSharp, trashOutline, trashSharp, warningOutline, warningSharp } from 'ionicons/icons';
+import SupabaseDataService from '../services/supabase.data.service'
 
-import { useLocation } from 'react-router-dom';
-import { archiveOutline, archiveSharp, bookmarkOutline, heartOutline, heartSharp, mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, trashOutline, trashSharp, warningOutline, warningSharp } from 'ionicons/icons';
+import info from '../../package.json';
+
 import './Menu.css';
+const supabaseDataService = SupabaseDataService.getInstance()
 
 interface AppPage {
   url: string;
   iosIcon: string;
   mdIcon: string;
   title: string;
+  showIf: boolean
 }
 
-const appPages: AppPage[] = [
-  {
-    title: 'Inbox',
-    url: '/page/Inbox',
-    iosIcon: mailOutline,
-    mdIcon: mailSharp
-  },
-  {
-    title: 'Outbox',
-    url: '/page/Outbox',
-    iosIcon: paperPlaneOutline,
-    mdIcon: paperPlaneSharp
-  },
-  {
-    title: 'Favorites',
-    url: '/page/Favorites',
-    iosIcon: heartOutline,
-    mdIcon: heartSharp
-  },
-  {
-    title: 'Archived',
-    url: '/page/Archived',
-    iosIcon: archiveOutline,
-    mdIcon: archiveSharp
-  },
-  {
-    title: 'Trash',
-    url: '/page/Trash',
-    iosIcon: trashOutline,
-    mdIcon: trashSharp
-  },
-  {
-    title: 'Spam',
-    url: '/page/Spam',
-    iosIcon: warningOutline,
-    mdIcon: warningSharp
-  }
-];
-
-const labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+let lastUserID: string | null = null;
 
 const Menu: React.FC = () => {
   const location = useLocation();
+	const history = useHistory();
+	const [ user, setUser ] = useState<User | null>(null);
+	const [ profile, setProfile ] = useState<any>(null);
+	const [ invites, setInvites ] = useState<any[]>([]);
+	const pages = useMemo(() => 
+	[
+		{
+			title: 'Dashboard',
+			url: '/dashboard',
+			iosIcon: barChartOutline,
+			mdIcon: barChartSharp,
+			showIf: true
+		},
+		{
+			title: 'Groups',
+			url: '/groups',
+			iosIcon: peopleOutline,
+			mdIcon: peopleSharp,
+			showIf: user !== null
+		},
+	], [user]);
 
-  return (
-    <IonMenu contentId="main" type="overlay">
-      <IonContent>
-        <IonList id="inbox-list">
-          <IonListHeader>Inbox</IonListHeader>
-          <IonNote>hi@ionicframework.com</IonNote>
-          {appPages.map((appPage, index) => {
-            return (
-              <IonMenuToggle key={index} autoHide={false}>
-                <IonItem className={location.pathname === appPage.url ? 'selected' : ''} routerLink={appPage.url} routerDirection="none" lines="none" detail={false}>
-                  <IonIcon slot="start" ios={appPage.iosIcon} md={appPage.mdIcon} />
-                  <IonLabel>{appPage.title}</IonLabel>
-                </IonItem>
+	const [ appPages, setAppPages ] = useState<AppPage[]>(pages);
+	const goToProfile = async () => {
+		history.replace('/profile');
+	}
+	const goToDashboard = async () => {
+		history.replace('/dashboard');
+	}
+	const onSignIn = (user: any, session: any) =>{
+		window.location.reload();
+	}
+	const onSignOut = () =>{
+		window.location.reload();
+	}
+    useEffect(() => {
+		const userSubscription = SupabaseAuthService.user.subscribe(setUser);
+		const profileSubscription = SupabaseAuthService.profile.subscribe(setProfile);
+  
+		return () => {
+			userSubscription.unsubscribe();
+			profileSubscription.unsubscribe();
+		}
+	  },[])
+	  useEffect(() => {
+		  if (lastUserID === user?.id) {
+			  return; // prevent looping, but looping will occur until setAppPages is finished
+		  }		  
+		  console.log('lastUserID', lastUserID);
+		  if (user && profile) {}
+		  setAppPages(pages);
+		  lastUserID = user?.id || null;
+		  if (user?.email) {
+			getMyInvitations(user?.email);
+		  } else {
+			  setInvites([]);
+		  }
+	}, [user, profile, pages])
+
+	const getMyInvitations = async (user_id: string) => {
+		if (!supabaseDataService.isConnected()) {
+		  await supabaseDataService.connect() // wait for db connection
+		}
+		const { data, error } = await supabaseDataService.getMyInvitations(user_id);
+		if (error) {
+			console.error('error getting my invitations', error)
+		} else {
+			console.log('getMyInvitations', data)
+			setInvites(data);
+		}
+	  }
+
+    return (
+      <IonMenu contentId='main' type='overlay'>
+        <IonContent>
+          <IonList id='inbox-list'>
+            <IonListHeader>Awana Info</IonListHeader>
+            <IonNote>tag line here<br/>v{info?.version}</IonNote>
+            <Login
+              SUPABASE_URL={process.env.REACT_APP_SUPABASE_URL || ''}
+              SUPABASE_KEY={process.env.REACT_APP_SUPABASE_KEY || ''}
+              // providers={['google', 'facebook', 'twitter', 'linkedin']}
+              backdropDismiss={false}
+              profileFunction={goToProfile}
+              onSignIn={onSignIn}
+              onSignOut={onSignOut}
+              profileTable={'profile'}
+              profileKey={'id'}
+              setUser={setUser}
+                />
+            <ResetPassword
+              SUPABASE_URL={process.env.REACT_APP_SUPABASE_URL || ''}
+              SUPABASE_KEY={process.env.REACT_APP_SUPABASE_KEY || ''}
+            />
+  
+            { invites.length > 0 &&
+              <div className="ion-padding">
+              <IonMenuToggle key={'pendingInvites'} autoHide={false}>
+                <IonButton
+                onClick={goToDashboard}
+                size='small'
+                expand='block'
+                color='danger'>
+                See Pending Invites</IonButton>
               </IonMenuToggle>
-            );
-          })}
-        </IonList>
-
-        <IonList id="labels-list">
-          <IonListHeader>Labels</IonListHeader>
-          {labels.map((label, index) => (
-            <IonItem lines="none" key={index}>
-              <IonIcon slot="start" icon={bookmarkOutline} />
-              <IonLabel>{label}</IonLabel>
-            </IonItem>
-          ))}
-        </IonList>
-      </IonContent>
-    </IonMenu>
-  );
-};
+              </div>
+            }
+            {appPages.map((appPage, index) => {
+              if (appPage.showIf) {
+              return (
+                <IonMenuToggle key={index} autoHide={false}>
+                  <IonItem
+                    className={location.pathname === appPage.url ? 'selected' : ''}
+                    routerLink={appPage.url}
+                    routerDirection='none'
+                    lines='none'
+                    detail={false}>
+                    <IonIcon slot='start' ios={appPage.iosIcon} md={appPage.mdIcon} />
+                    <IonLabel>{appPage.title}</IonLabel>
+                  </IonItem>
+                </IonMenuToggle>
+              )} else {
+                return (null)						
+              }
+            })}
+          </IonList>
+          {/* <pre>
+            { JSON.stringify(invites, null, 2) }
+          </pre> */}
+        </IonContent>
+      </IonMenu>
+    )
+  };
 
 export default Menu;
---
+++ src/services/supabase.data.service.ts
@@ -0,0 +1,178 @@
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
+
+let supabase: SupabaseClient;
+
+export default class SupabaseDataService {
+	static myInstance:any = null;
+
+	static getInstance() {
+		if (this.myInstance === null) {
+      console.log('********************************************');
+      console.log('Creating new instance of SupabaseDataService');
+      console.log('********************************************');
+		  this.myInstance = new this();
+      this.myInstance.connect();
+		}
+		return this.myInstance;
+	}
+  
+  public isConnected = () => {
+    return (typeof supabase !== 'undefined');
+  }
+  public getSupabase = () => {
+    return supabase;
+  }
+  public connect = async () => {
+      if (this.isConnected()) { return; }
+      supabase = await createClient(
+        process.env.REACT_APP_SUPABASE_URL || '', 
+        process.env.REACT_APP_SUPABASE_KEY || '');
+  }
+
+  public getGroup = async (id: string) => {
+    console.log('getGroup', id);
+    console.log('supabase', supabase);
+    const { data, error } = await supabase
+    .from('groups')
+    .select('*')
+    .eq('id', id)
+    .limit(1)
+    .single();
+    return { data, error };
+  }
+  public saveGroup = async (group: any) => {
+    const { data, error } = await supabase
+    .from('groups')
+    .upsert(group);
+    return { data, error };
+  }
+  public deleteGroup = async (id: string) => {
+    const { data, error } = await supabase
+    .rpc('groups_delete', {"target": id});
+    return { data, error };
+  }
+  public getGroups = async (user_id: string) => {
+    const { data, error } = await supabase
+    .from('groups_access')
+    .select('groups(*)')
+    .eq('user_id', user_id);
+    let groups;     
+    if (data && data.length > 0) {
+      groups = [];
+      for (let i=0; i<data.length; i++) {
+        groups.push(data[i].groups);
+      }
+    }
+    return { data: groups, error };
+  }
+  public hasChildGroups = async (id: string) => {
+    console.log('hasChildGroups looing for parent_id', id);
+    const { error, count } = await supabase
+    .from('groups')
+    .select('id', { count: 'exact', head: true })
+    .eq('parent_id', id);
+    console.log('hasChildGroups, data, error, count', error, count);
+    if (error) {
+      console.error('hasChildGroups error', error);
+      return { error };
+    } else {
+      return count;
+    }
+  }
+
+  public async getProfile(id: string) {
+    if (id) {
+      const { data, error } = 
+      await supabase.from('profile')
+      .select('*')
+      .eq('id', id)
+      .limit(1)
+      .single(); // return a single object (not an array)
+      return { data, error };  
+    } else {
+      console.error('#### getProfile: no id');
+      return { data: {}, error: null };  
+    }
+  } 
+
+  public async saveProfile(profile: any) {
+    const { data, error } = 
+    await supabase.from('profile')
+    .upsert(profile);
+    return { data, error };
+  }
+
+  public async groups_get_tree_for_group(id: string) {
+    const { data, error } =
+    await supabase.rpc('groups_get_tree_for_group', {target_group_id: id});
+    return { data, error };
+  }
+  public async groups_get_groups_for_user(user_id: string) {
+    const { data, error } =
+    await supabase.rpc('groups_get_groups_for_user', {target_user_id: user_id});
+    return { data, error };
+  }
+  public async getInvitations(group_id: string) {
+    console.log('data service getInvitations', group_id);
+    const { data, error } = await supabase
+    .from('invitations')
+    .select('*')
+    .eq('group_id', group_id)
+    //.eq('result', null);
+    return { data, error };
+  }
+  public async getMyInvitations(email: string) {
+    const { data, error } = await supabase
+    .from('invitations')
+    .select('*, groups(*)')
+    .eq('email', email)
+    .is('closed_at', null);
+    return { data, error };
+  }
+  public async invitations_accept(id: string) {
+    console.log('invitations_accept, calling rpc invitations_accept');
+    const { data, error } = await supabase
+    .rpc('invitations_accept', {target: id});
+    console.log('invitations_accept, got data, error', data, error);
+    return { data, error };
+  }
+  public async invitations_reject(id: string) {
+    const { data, error } = await supabase
+    .rpc('invitations_reject', {target: id});
+    return { data, error };
+  }
+  public async inviteUsersToGroup(current_user_id: string, group_id: string, userEmails: string[], inviteAccess: string) {
+    const data = [];
+    for (let i=0; i<userEmails.length; i++) {
+      const invite: any = {group_id, email: userEmails[i], access: inviteAccess, invited_by: current_user_id};
+      const { data: lookupData, error: lookupError } = await supabase
+      .from('invitations')
+      .select('id')
+      .eq('group_id', group_id)
+      .eq('email', userEmails[i])
+      .limit(1);
+      console.log('lookupData, lookupError', lookupData, lookupError);
+      if (lookupError) {
+        console.error('inviteUsersToGroup lookup error', lookupError);
+        return { error: lookupError };
+      }
+      if (lookupData && lookupData.length && lookupData[0].id) {
+        invite.id = lookupData[0].id;
+      }
+      const { data: saveData, error: saveError } = await supabase
+      .from('invitations')
+      .upsert(invite);
+      if (saveError) {
+        console.error('inviteUsersToGroup save error', saveError);
+        return { error: saveError };
+      } else {
+        data.push(saveData[0]);
+      }
+      console.log('inviteUsersToGroup saveData, saveError', saveData, saveError);
+
+
+    }
+    return { data, error: null };
+  }
+
+}

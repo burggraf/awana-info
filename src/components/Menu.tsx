dfbd609695dfaab627a4a4a2f7c8a255b7209d51
Mark Burggraf <markb@mantisbible.com>
2022-06-07 07:39:20 -0700

    added Facebook and Google login

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -114,6 +114,7 @@ const Menu: React.FC = () => {
             <Login
               SUPABASE_URL={process.env.REACT_APP_SUPABASE_URL || ''}
               SUPABASE_KEY={process.env.REACT_APP_SUPABASE_KEY || ''}
+              providers={['facebook','google']}
               // providers={['google', 'facebook', 'twitter', 'linkedin']}
               backdropDismiss={false}
               profileFunction={goToProfile}

83a1942938a0006db00e476532d4c9a151db9ca2
Mark Burggraf <markb@mantisbible.com>
2022-06-13 11:13:44 -0700

    change to gridjs-react for people list

--- src/pages/People.tsx
+++ src/pages/People.tsx
@@ -2,8 +2,9 @@ import { IonButton, IonButtons, IonCol, IonContent, IonGrid, IonHeader, IonIcon,
 // import { User } from '@supabase/supabase-js'
 //import { SupabaseAuthService } from 'ionic-react-supabase-login';
 import { addOutline, addSharp, peopleOutline, peopleSharp } from 'ionicons/icons';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useHistory } from 'react-router';
+import { Grid } from 'gridjs-react'
 
 import { Person as PersonObject } from '../../models/Person'
 import SupabaseDataService from '../services/supabase.data.service'
@@ -18,8 +19,11 @@ const supabaseDataService = SupabaseDataService.getInstance()
 
 const People: React.FC = () => {
     const history = useHistory();
+    const peopleGrid: any = useRef(null)
+
     const [showLoading, setShowLoading] = useState(true);
-    const [ people, setPeople ] = useState<PersonObject[]>([]);
+    // const [ people, setPeople ] = useState<PersonObject[]>([]);
+    const [ people, setPeople ] = useState<any[]>([]);
 
     // const [ user, setUser ] = useState<any>(null);
     // const [ profile, setProfile ] = useState<any>(null);
@@ -31,17 +35,45 @@ const People: React.FC = () => {
             console.error('loadPeople: error', error)
         } else {
             setPeople(data);
+            console.log('peoplel set to',data);
+
         }
         setShowLoading(false);
       }
       loadPeople();
       // const userSubscription = SupabaseAuthService.subscribeUser(setUser);
       // const profileSubscription = SupabaseAuthService.subscribeProfile(setProfile);
+      if (peopleGrid?.current?.instance!) {
+        console.log('setting rowClickHandler');
+        peopleGrid.current.instance.on('rowClick', (...args: any[]) => {
+          const cells = args[1].cells;
+          // console.log('args', args);
+          // for (let i=0; i < args[1].cells.length; i++) {
+          //   console.log(i, args[1].cells[i].data);
+          // }
+          history.push(`/person/${cells[0].data}`);
+        });
+      }
       return () => {
           // SupabaseAuthService.unsubscribeUser(userSubscription);
           // SupabaseAuthService.unsubscribeProfile(profileSubscription);
       }
     },[])
+
+    // useEffect(() => {
+    //   if (peopleGrid?.current?.instance!) {
+    //     console.log('setting rowClickHandler');
+    //     peopleGrid.current.instance.on('rowClick', (...args: any[]) => {
+    //       console.log('args', args);
+    //       console.log('id', args[1].id);
+    //       for (let i=0; i < args[1].cells.length; i++) {
+    //         console.log(i, args[1].cells[i].data);
+    //       }
+    //       history.push(`/person/${args[1].cells[0].data}`)
+    //     });
+    //   }
+    // }, [people])
+  
    
     return (
     <IonPage>
@@ -65,6 +97,23 @@ const People: React.FC = () => {
       <IonContent class="ion-padding">
 
       <IonLoading isOpen={showLoading} message="Loading" />
+
+      <Grid
+              key={'peopleGrid'}
+							ref={peopleGrid}
+							data={people}
+							columns={[
+                {name:'id',id:'id',hidden:true},
+                {name:'Firstname',id:'firstname'},
+                {name:'Lastname',id:'lastname'},
+							]}
+							search={true}
+							sort={true}
+							pagination={{
+								enabled: true,
+								limit: 15,
+							}}
+						/>
 
         <IonGrid key={'peopleList'}>
             {people.map((person: PersonObject) => {
